348eb58c6a0b2c9b09bdd5cf208c091cb62e3890
基督徒.羅小雪 <snowray712000@gmail.com>
2021-04-23 15:49:43 +0800

    Update IjnTestTime.swift

--- IjnTestTime.swift
+++ IjnTestTime.swift
@@ -38,3 +38,26 @@ extension IjnTestTime {
         return m * 1000.0
     }
 }
+extension IjnTestTime {
+    /// 測時間，有時候第1個會特別久，因此開發這個作參考。
+    public static func getAvgQ1toQ3() -> Double {
+        
+        if dts.count == 0 { return Double.nan }
+        else if dts.count == 1 { return dts.first! }
+        else if dts.count == 2 { return (dts[0]+dts[1]) / 2.0}
+        else if dts.count == 3 { return dts.sorted()[1] }
+        
+        let di = 4.0 / Double(dts.count)
+        let i1 = Int ( di + 0.5 )
+        let i3 = Int ( di * 3 + 0.5 )
+        
+        
+        let v2 = dts.sorted()
+        var sum = 0.0
+        for i in i1..<i3 + 1 {
+            sum += v2[i]
+        }
+        sum /= Double((i3-i1+1))
+        return sum
+    }
+}
